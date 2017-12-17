57322ba65610145dc6679ee7d948e6dc9f6eb09a
olow304 <olow304@gmail.com>
2017-12-17 13:48:24 -0500

    data exploring and OSL

--- Data Exploring/osl_model.py
+++ Data Exploring/osl_model.py
@@ -1,2 +1,76 @@
-sns.countplot(x='y', data=data, palette='hls')
-plt.show()
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 14 10:57:11 2017
+
+@author: Saleban
+"""
+#Multiple Linear Regression
+
+#Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Data
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4]
+
+#Categorical variables
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder = LabelEncoder()
+X[:, 3] = labelencoder.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features=[3])
+X = onehotencoder.fit_transform(X).toarray()
+
+#Avoiding dummy variable trap
+X = X[:, 1:]
+
+#Fitting train and test sets
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/5, random_state = 0)
+
+#Fitting multiple linear regression
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+#Predict test set result
+y_pred = regressor.predict(X_test)
+
+#Building the optimal model using backward elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50,1)).astype(int),values = X, axis = 1) 
+#Let's assume that our Significance leve = 0.05
+#Fit the model with all possible prediction
+
+# here you start with all features...
+# and you keep removing, least predicted feature...
+X_opt = X[:,[0,1,2,3,4,5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+#Remove the predictor - P > SL
+X_opt = X[:,[0,1,3,4,5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+#Remove the predictor - P > SL
+X_opt = X[:,[0,3,4,5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+#Remove the predictor - P > SL
+X_opt = X[:,[0,3,5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+#Remove the predictor - P > SL
+# at the end you only left, some important features that tell more about your predicted model...
+X_opt = X[:,[0,3]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+
+
+
